08ae41c2d35c997c97b8eecfcbc5d1e13230b4d6
Tristan Leonard <tristanl@slalom.com>
2024-04-22 15:45:45 -0700

    app-root log

--- log-sidecar.sh
+++ log-sidecar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p /var/log/flask-app/
-touch /var/log/flask-app/app.log
-tail -f /var/log/flask-app/app.log
+mkdir -p /opt/app-root/logs/
+touch /opt/app-root/logs/app.log
+tail -f /opt/app-root/logs/app.log
